8f4adccaa3b2b12f6abc14bd160df45127cfa13d
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2018-08-02 14:30:35 +0100

    [JBEAP-15168] Propagating undertow authentication mode to Elytron

--- undertow/src/main/java/org/wildfly/extension/undertow/ApplicationSecurityDomainDefinition.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/ApplicationSecurityDomainDefinition.java
@@ -514,6 +514,7 @@ public class ApplicationSecurityDomainDefinition extends PersistentResourceDefin
                     .setNext(toWrap)
                     .setSecurityDomain(httpAuthenticationFactory.getSecurityDomain())
                     .setMechanismSupplier(() -> getAuthenticationMechanisms(selectedMechanisms))
+                    .setAuthenticationMode(deploymentInfo.getAuthenticationMode())
                     .setHttpExchangeSupplier(httpServerExchange -> new ElytronHttpExchange(httpServerExchange, scopeResolvers, scopeSessionListener) {
                         @Override
                         protected SessionManager getSessionManager() {
