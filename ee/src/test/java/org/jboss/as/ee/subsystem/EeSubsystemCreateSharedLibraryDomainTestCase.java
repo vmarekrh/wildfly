69a75e10449448bf35efbf9278e831263d2cf625
Vratislav Marek <vmarek@redhat.com>
2019-04-26 09:45:50 +0200

    Add Test Cases for Java EE subsystem for include shared library

---
+++ ee/src/test/java/org/jboss/as/ee/subsystem/EeSubsystemCreateSharedLibraryDomainTestCase.java
@@ -0,0 +1,14 @@
+package org.jboss.as.ee.subsystem;
+
+import org.junit.Test;
+
+/**
+ * @author Vratislav Marek (vmarek@redhat.com)
+ **/
+public class EeSubsystemCreateSharedLibraryDomainTestCase {
+
+    @Test
+    public void testAppSharedLib(){
+
+    }
+}
